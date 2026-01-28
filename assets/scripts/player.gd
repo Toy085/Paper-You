148039dcc2dbb9fcafdef17c599f468357acc9c5
Toy085 <brightabaka1@gmail.com>
2026-01-28 11:11:55 +0100

    Added squish and squash animation to jump

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 7.0
 @export var jump_velocity = 6.0
-@export var flip_speed = 15.0 
+@export var flip_speed = 15.0
 
 @onready var sprite = $Sprite3D
 
@@ -32,5 +32,10 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-
+	
+	if not is_on_floor():
+		sprite.scale.y = lerp(sprite.scale.y, 0.75, delta * 6)
+	elif is_on_floor():
+		sprite.scale.y = lerp(sprite.scale.y, 1.0, delta * 6)
+	
 	move_and_slide()
