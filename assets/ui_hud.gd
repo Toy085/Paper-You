2ad845dcc50d73a86ff19561324c59001461afd3
Toy085 <brightabaka1@gmail.com>
2026-02-06 16:26:38 +0100

    Added hover animation to coin

--- assets/ui_hud.gd
+++ assets/ui_hud.gd
@@ -11,5 +11,5 @@ func _update_coin_ui(amount: int):
 	
 	# Coin text pop animation
 	var tween = create_tween()
-	tween.tween_property(coin_label, "scale", Vector2(1.2, 1.2), 0.1).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+	tween.tween_property(coin_label, "scale", Vector2(1.1, 1.1), 0.1).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 	tween.tween_property(coin_label, "scale", Vector2(1.0, 1.0), 0.2)
--- entities/coin.gd
+++ entities/coin.gd
@@ -6,13 +6,17 @@ var value = 1
 
 func _ready():
 	# Spin animation
-	var tween = create_tween().set_loops()
-	tween.tween_property(coinModell, "rotation:y", deg_to_rad(360), 1.5).from(0)
+	var spin_tween = create_tween().set_loops()
+	spin_tween.tween_property(coinModell, "rotation:y", deg_to_rad(360), 1.5).from(0)
+
+	# Hover animation
+	var hover_tween = create_tween().set_loops()
+	hover_tween.tween_property(coinModell, "position:y", 0.3, 0.8).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	hover_tween.tween_property(coinModell, "position:y", 0.0, 0.8).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
 
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	print("Coin touched by: ", body.name)
 	if body.is_in_group("player"):
 		GameEvents.add_money(value)
 		# Add a sound or particle effect here!
