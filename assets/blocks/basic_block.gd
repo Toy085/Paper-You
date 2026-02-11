b36a29c5b313eb119b6eddb849dd661faadc2d79
Toy085 <brightabaka1@gmail.com>
2026-02-11 18:32:32 +0100

    Fixed coin block

--- assets/blocks/basic_block.gd
+++ assets/blocks/basic_block.gd
@@ -1,9 +1,11 @@
 extends StaticBody3D
 
+var value = 1.0
+
 func on_hammer_hit():
-	print("I got hit by the hammer!")
 	
 	# Paper Mario Juice: Make the block bounce up and down
 	var tween = create_tween()
-	tween.tween_property(self, "position:y", position.y + 0.5, 0.1) # Up
+	tween.tween_property(self, "position:y", position.y + 0.5, 0.05) # Up
 	tween.tween_property(self, "position:y", position.y, 0.1)     # Down
+	GameEvents.add_money(value)
