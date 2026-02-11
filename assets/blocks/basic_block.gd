ccadfde26c5dd0540fcc9155365e02cb0f7dff39
Toy085 <brightabaka1@gmail.com>
2026-02-11 19:00:51 +0100

    Updated player collison shape

--- assets/blocks/basic_block.gd
+++ assets/blocks/basic_block.gd
@@ -2,10 +2,20 @@ extends StaticBody3D
 
 var value = 1.0
 
+@export_enum("coin", "save", "normal-delete", "normal-bounce", "item") var type: String = "coin"
+
 func on_hammer_hit():
 	
 	# Paper Mario Juice: Make the block bounce up and down
 	var tween = create_tween()
 	tween.tween_property(self, "position:y", position.y + 0.5, 0.05) # Up
 	tween.tween_property(self, "position:y", position.y, 0.1)     # Down
-	GameEvents.add_money(value)
+	match type:
+		"coin":
+			GameEvents.add_money(value)
+		"save":
+			print("Saving game...")
+		"item":
+			print("Giving item...")
+		"normal-delete":
+			queue_free()
